29e6e8d26fd0eb44ef2fe2b33cfa2f75e97533d5
MaximilianoWenner <88354878+MaximilianoWenner@users.noreply.github.com>
2026-09-16 09:25:04 -0300

    Add files via upload

--- Semana 05/Presencial/Introduccion archivos/main.cpp
+++ Semana 05/Presencial/Introduccion archivos/main.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+/**
+Crear un programa con un menú con las siguientes opciones:
+1-Agregar un registro al archivo.
+2-Listar los registros del archivo.
+EXTRA:
+Hacer una opción que reciba una posición y liste el registro de esa posición (el primer registro de mi archivo ocupa la posición 0).
+
+TAREA:
+Agregar una opción al menú que me permita listar un alumno que tenga un legajo que se ingresa por teclado.
+
+DIFICIL:
+Listar a todos los alumnos que tengan una fecha de nacimiento anterior a una fecha que se ingresa por teclado.
+*/
+
+
 int main()
 {
     Alumno obj;
@@ -22,16 +37,23 @@ int main()
         return -1;
     }
     ///GUARDO INFORMACION EN MI ARCHIVO
-//    fwrite(&obj, sizeof obj, 1, p);
     obj.setNombreApellido("PISO EL NOMBRE");
     while(fread(&obj, sizeof obj, 1, p)==1){
         obj.Mostrar();
         cout<<"====================="<<endl;
     }
+//    fwrite(&obj, sizeof obj, 1, p);
     /**
     Primer argumento: la dirección de memoria donde está la información que quiero copiar en mi archivo.
     Segundo argumento: la cantidad de bytes que quiero copiar a mi archivo.
     Tercer argumento: la cantidad de registros que quiero copiar en mi archivo.
+    Cuarto argumento: el vínculo con mi archivo (puntero FILE).
+    */
+    //fread(&obj, sizeof obj, 1, p);
+    /**
+    Primer argumento: la dirección de memoria a donde quiero copiar la información de mi archivo.
+    Segundo argumento: la cantidad de bytes que quiero leer de mi archivo.
+    Tercer argumento: la cantidad de registros que quiero leer de mi archivo.
     Cuarto argumento: el vínculo con mi archivo (puntero FILE).
     */
     ///CIERRO EL VINCULO CON MI ARCHIVO
