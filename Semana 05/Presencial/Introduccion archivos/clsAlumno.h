a1af5a10cf82b44146804850ff5b5e7559a9ab1b
MaximilianoWenner <88354878+MaximilianoWenner@users.noreply.github.com>
2026-09-16 09:16:15 -0300

    Add files via upload

--- Semana 05/Presencial/Introduccion archivos/clsAlumno.h
+++ Semana 05/Presencial/Introduccion archivos/clsAlumno.h
@@ -3,13 +3,13 @@
 
 class Alumno{
     private:
-        int legajo;
-        char nombreApellido[50];
-        int dni;
-        int diaNacimiento;
-        int mesNacimiento;
-        int anioNacimiento;
-        char domicilio[50];
+        int legajo; ///4
+        char nombreApellido[50]; ///50
+        int dni; ///4
+        int diaNacimiento; ///4
+        int mesNacimiento; ///4
+        int anioNacimiento; ///4
+        char domicilio[50]; ///50
     public:
         Alumno(int l=1, const char *nA="S/N", int d=0, int dN=1, int mN=1, int aN=1900, const char *dom="S/D");
         void Cargar();
--- Semana 05/Presencial/Introduccion archivos/main.cpp
+++ Semana 05/Presencial/Introduccion archivos/main.cpp
@@ -5,23 +5,35 @@ using namespace std;
 
 int main()
 {
-    Alumno obj(12345, "Juan Manuel Fernandez", 44000333, 24, 4, 2002, "Calle falsa  123");
-    obj.Mostrar();
+    Alumno obj;
+//    obj.Mostrar();
+    //obj.Cargar();
     ///CREO UN PUNTERO FILE QUE VA A SER EL VINCULO CON MI ARCHIVO
     FILE *p;
     ///ABRO EL ARCHIVO
-    p = fopen("alumnos.dat", "wb");
+    p = fopen("alumnos.dat", "rb");
     ///MODOS DE APERTURA:
     ///LA b INDICA QUE ES UN ARCHIVO BINARIO
     ///r (read): ABRE EL ARCHIVO EN MODO LECTURA. SI NO EXISTE, NO LO CREA.
-    ///a (append): ABRE EL ARCHIVO EN MODO ESCRITURA. SI NO EXISTE, LO CREA.
+    ///a (append): ABRE EL ARCHIVO EN MODO ESCRITURA. SI NO EXISTE, LO CREA. SIEMPRE ESCRIBE AL FINAL.
     ///w (write): ABRE EL ARCHIVO EN MODO ESCRITURA. SIEMPRE CREA UN ARCHIVO EN BLANCO.
     if(p == nullptr){
         cout<<"ERROR DE APERTURA DE ARCHIVO"<<endl;
         return -1;
     }
     ///GUARDO INFORMACION EN MI ARCHIVO
-    fwrite(&obj, sizeof obj, 1, p);
+//    fwrite(&obj, sizeof obj, 1, p);
+    obj.setNombreApellido("PISO EL NOMBRE");
+    while(fread(&obj, sizeof obj, 1, p)==1){
+        obj.Mostrar();
+        cout<<"====================="<<endl;
+    }
+    /**
+    Primer argumento: la dirección de memoria donde está la información que quiero copiar en mi archivo.
+    Segundo argumento: la cantidad de bytes que quiero copiar a mi archivo.
+    Tercer argumento: la cantidad de registros que quiero copiar en mi archivo.
+    Cuarto argumento: el vínculo con mi archivo (puntero FILE).
+    */
     ///CIERRO EL VINCULO CON MI ARCHIVO
     fclose(p);
     return 0;
